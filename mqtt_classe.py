e5ca1e7f12ada7595137166955ce2c636ab1d972
Gabriel Bonaretti <114812017+GabrielBonaretti@users.noreply.github.com>
2023-04-13 11:21:14 -0300

    Update mqtt_classe.py

--- mqtt_classe.py
+++ mqtt_classe.py
@@ -1,16 +1,22 @@
-import datetime
 from random import randint
 from paho.mqtt import client as mqtt_client
 
 
 class Mqtt:
     def __init__(self):
-        self.broker = 'broker.emqx.io'
+        '''
+
+        '''
+        self.broker = '10.21.160.16'
         self.port = 1883
         self.topic = "python/mqtt"
         self.client_id = f'python-mqtt-{randint(0, 1000)}'
 
     def connect_mqtt(self):
+        '''
+
+        :return:
+        '''
         def on_connect(client, userdata, flags, rc):
             if rc == 0:
                 pass
@@ -23,11 +29,17 @@ class Mqtt:
         client.connect(self.broker, self.port)
         return client
 
-    def publish(self, client, escolha):
+    def publish(self, client, escolha, msg):
+        '''
+
+        :param client:
+        :param escolha:
+        :return:
+        '''
         if escolha == 1:
             msg_count = input("Digite seu nome: ")
         elif escolha == 2:
-            msg_count = input("Digite a posição: ")
+            msg_count = msg
         while True:
             result = client.publish(self.topic, msg_count)
             status = result[0]
@@ -38,6 +50,12 @@ class Mqtt:
                 print(f"Failed to send message to topic {self.topic}")
 
     def subscribe_nomes(self, client: mqtt_client, lista_nomes):
+        '''
+
+        :param client:
+        :param lista_nomes:
+        :return:
+        '''
         def on_message(client, userdata, msg):
             lista_nomes.append(msg.payload.decode())
             client.disconnect()
@@ -46,6 +64,12 @@ class Mqtt:
         client.on_message = on_message
 
     def subscribe_posicao(self, client: mqtt_client, lista_posicao):
+        '''
+
+        :param client:
+        :param lista_posicao:
+        :return:
+        '''
         def on_message(client, userdata, msg):
             lista_posicao.append(int(msg.payload.decode()))
             print(lista_posicao)
@@ -55,26 +79,49 @@ class Mqtt:
         client.on_message = on_message
 
     def recebe_nomes(self, lista_nomes, teste):
+        '''
+
+        :param lista_nomes:
+        :param teste:
+        :return:
+        '''
         self.subscribe_nomes(teste, lista_nomes=lista_nomes)
         teste.loop_forever()
 
     def recebe_posicao(self, teste, lista_posicao):
+        '''
+
+        :param teste:
+        :param lista_posicao:
+        :return:
+        '''
         self.subscribe_posicao(teste, lista_posicao=lista_posicao)
         teste.loop_forever()
 
-    def manda_nomes(self, lista_nomes, teste, escolha):
+    def manda_nomes(self, lista_nomes, teste, escolha, msg=''):
+        '''
+
+        :param lista_nomes:
+        :param teste:
+        :param escolha:
+        :return:
+        '''
         teste.loop_start()
-        x, nome = self.publish(teste, escolha=escolha)
+        x, nome = self.publish(teste, escolha=escolha, msg=msg)
         if x == 0:
             teste.loop_stop()
         lista_nomes.append(nome)
 
-    def manda_posicao(self, lista_posicao, teste, escolha):
+    def manda_posicao(self, lista_posicao, teste, escolha, msg):
+        '''
+
+        :param lista_posicao:
+        :param teste:
+        :param escolha:
+        :return:
+        '''
         teste.loop_start()
-        x, nome = self.publish(teste, escolha=escolha)
+        x, nome = self.publish(teste, escolha=escolha, msg=msg)
         if x == 0:
             teste.loop_stop()
         lista_posicao.append(int(nome))
-
-
-
