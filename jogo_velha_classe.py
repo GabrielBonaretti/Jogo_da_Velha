8eafb01d1b763ced60093f9c243a799b1697fc08
Gabriel Bonaretti <114812017+GabrielBonaretti@users.noreply.github.com>
2023-04-13 11:21:34 -0300

    Update jogo_velha_classe.py

--- jogo_velha_classe.py
+++ jogo_velha_classe.py
@@ -4,6 +4,15 @@ from random import randint
 
 class Jogo:
     def __init__(self, tabuleiro='', x=True, jogador_1='x', jogador_2='o', escolha_jogador=0):
+        '''
+        Método construtor da classe jogo
+
+        :param tabuleiro: tabuleiro do jogo
+        :param x: variavel para decidir se apenas ira printar ou acabar  jogo
+        :param jogador_1: se jogador 1 é 'x' ou 'o'
+        :param jogador_2: se jogador 2 é 'x' ou 'o'
+        :param escolha_jogador: contador para escolhar qual jogador jogará
+        '''
         tabuleiro = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
         self.__tabuleiro = tabuleiro
         self.__x = x
@@ -12,6 +21,11 @@ class Jogo:
         self.__escolha_jogador = escolha_jogador
 
     def jogo_contra_robo(self):
+        '''
+        Função caso o usuário final escolha jogar contra o rôbo
+
+        :return: jogo contra rôbo
+        '''
         self.__escolha_xoub()
         self.print_jogo()
 
@@ -23,6 +37,11 @@ class Jogo:
         os.system('cls')
 
     def jogo_contra_usuarios(self):
+        '''
+        Função caso o usuário final escolha jogar contra outro usuário no mesmo computador
+
+        :return: Jogo dois usuários um computador
+        '''
         self.__escolha_xoub()
         self.print_jogo()
 
@@ -33,10 +52,21 @@ class Jogo:
 
     @property
     def jogador_1(self):
+        '''
+        Método get da atributo jogador_1
+
+        :return: valor jogador_1
+        '''
         return self.__jogador_1.lower()
 
     @jogador_1.setter
     def jogador_1(self, escolha):
+        '''
+        Método set da atributo do jogador_1
+
+        :param escolha: escolha se o jogador um jogara com 'x' ou 'o'
+        :return: se jogador 1 escolher 'x' o jogador 2 será 'o', assim vice-versa
+        '''
         if escolha == 'x':
             self.__jogador_1 = escolha
             self.__jogador_2 = 'o'
@@ -47,6 +77,10 @@ class Jogo:
             print("Tente novamente")
 
     def __escolha_xoub(self):
+        '''
+        pergunta ao usuário final qual será o símbolo que ele jogará 'x' ou  'o'
+        :return: o código retornará a pergunta até que o usuário escolha o símbolo 'x' ou 'o'
+        '''
         while True:
             self.jogador_1 = input("Digite qual simbolo voce deseja para o jogador 1 [x/o]: ")
 
@@ -54,6 +88,10 @@ class Jogo:
                 break
 
     def print_jogo(self):
+        '''
+        Printa o tabuleiro na tela
+        :return: Printa o tabuleiro na tela
+        '''
         for i in range(9):
             if i > 0 and i % 3 == 0:
                 print()
@@ -62,18 +100,41 @@ class Jogo:
             print(self.__tabuleiro[i], end=" | ")
         print()
 
-    def __fim_jogo(self):
+    def fim_jogo(self, jogador1, jogador2):
+        '''
+        Método para a última ação do jogo, quando uma fileira ja foi completa e o resta ápenas mostrar ao usuário
+
+        :return: printa o tabuleiro juntamente com a mensagem fim de jogo
+        '''
         self.print_jogo()
         print("fim jogo")
+
+        if self.__escolha_jogador % 2 == 0:
+            print("O {} ganhou.".format(jogador2))
+        elif self.__escolha_jogador % 2 == 1:
+            print("O {} ganhou.".format(jogador1))
+
         exit()
 
-    def decidindo_print(self):
+    def decidindo_print(self, jogador1="jogador 1", jogador2='jogador 2'):
+        '''
+        Decide com o valor de x se ele terminará o jogo ou apenas irá printar o tabuleiro
+
+        :return: caso x for false o código chama o método de fim de de jogo, caso não ele so printa o tabuleiro
+        '''
         if self.__x is False:
-            self.__fim_jogo()
+            self.fim_jogo(jogador1=jogador1, jogador2=jogador2)
         else:
             self.print_jogo()
 
     def __chute_usuario(self):
+        '''
+        Mostra que jogador deve jogar e pergunta em qual posição ele joga, além de se é possível jogar nessa
+        posição, so saindo do método quando o jogador escrever uma posição válida.
+
+        :return: método para o jogo de usuário contra usuário no mesmo computador, no qual pergunta a posição e tambem
+                 verifica se é possível jogal lá
+        '''
         while True:
             if self.__escolha_jogador % 2 == 0:
                 print("Jogador 1: ")
@@ -83,8 +144,8 @@ class Jogo:
             posicao = int(input("Digite a posicao: "))
 
             if self.__tabuleiro[posicao] == 'x' or self.__tabuleiro[posicao] == 'o':
-                print("Você é burro?")
-
+                print("Ja tem ai usuário final")
+                
             elif self.__escolha_jogador % 2 == 0:
                 self.__tabuleiro[posicao] = self.__jogador_1
                 break
@@ -94,17 +155,26 @@ class Jogo:
                 break
 
     def __chute_robo(self):
-        i = 0
+        '''
+        Pergunta em qual posição ele joga, além de se é possível jogar nessa posição, saindo do método quando o jogador
+        escrever uma posição válida. Nesse método o chute do rôbo é feito de maneira aleatória e verificada se possível
+        a jogada do mesmo, caso não seja ele sorteia outro número
+
+        :return: Método para o jogo de usuário contra rôbo no mesmo computador, no qual pergunta a posição e tambem
+                 verifica se é possível jogal lá
+        '''
+
         while True:
             posicao = int(input("Digite a posicao: "))
 
             if self.__tabuleiro[posicao] == 'x' or self.__tabuleiro[posicao] == 'o':
-                print("Você é burro?")
+                print("Ja tem ai usuário final")
 
             else:
                 self.__tabuleiro[posicao] = self.__jogador_1
                 break
 
+        i = 0
         while i < 9:
             posicao_sorteada = randint(0, 8)
 
@@ -115,6 +185,10 @@ class Jogo:
                 break
 
     def verificar_jogo(self):
+        '''
+        Esse método teste todas as possibilidades de termino do jogo da velha e adiciona um no contador em cada jogada.
+        :return: Retor falso na variável x caso algum
+        '''
         if self.__tabuleiro[0] == "x" and self.__tabuleiro[1] == "x" and self.__tabuleiro[2] == "x" or \
                 self.__tabuleiro[0] == "o" and self.__tabuleiro[1] == "o" and self.__tabuleiro[2] == "o":
             self.__x = False
@@ -152,9 +226,18 @@ class Jogo:
 
     @property
     def escolha_jogador(self):
+        '''
+        Método get da atributo escolha_jogador
+        :return: Valor escolha_jogador
+        '''
         return self.__escolha_jogador
 
     def jogar_mqtt(self, posicao):
+        '''
+        Pega a posição que recebeu do mqtt e testa se é possivel fazer a jogada.
+        :param posicao: posição da jogada que é recebida pelo mqtt
+        :return: o simbolo do jogador 1 ou jogador 2 (selecao feita a partir da escolha do jogador) na posição recebida
+        '''
         while True:
             if self.__tabuleiro[posicao] == 'x' or self.__tabuleiro[posicao] == 'o':
                     print("Você é burro?")
@@ -166,3 +249,22 @@ class Jogo:
             elif self.__escolha_jogador % 2 == 1:
                 self.__tabuleiro[posicao] = self.__jogador_2
                 break
+    
+    @property
+    def teste_posicao(self):
+        while True:
+            try:
+                posicao = int(input("Digite a posicao: "))
+
+                if self.__tabuleiro[posicao] == 'x' or self.__tabuleiro[posicao] == 'o':
+                    print("Ja tem ai usuário final")
+                    
+                elif posicao < 0 or posicao > 8:
+                    print("Número fora do range")
+                    
+                else:
+                    return posicao
+                    break
+                
+            except:
+                print("Só número")
