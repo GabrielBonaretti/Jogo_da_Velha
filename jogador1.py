4886d231abefea4bfe109e01dbde815768649037
Gabriel Bonaretti <114812017+GabrielBonaretti@users.noreply.github.com>
2023-04-13 11:19:45 -0300

    Update jogador1.py

--- jogador1.py
+++ jogador1.py
@@ -1,14 +1,15 @@
 import os
+import time
 
 from jogo_velha_classe import Jogo
 from mqtt_classe import Mqtt
 
 
 def main():
-    jogo_objeto = Jogo()
-    mqtt_objeto = Mqtt()
-
     while True:
+        jogo_objeto = Jogo()
+        mqtt_objeto = Mqtt()
+    
         print("Modo de jogo:")
         print("[1]Jogo contra robo")
         print("[2]Dois jogadores")
@@ -35,25 +36,27 @@ def main():
                 mqtt_objeto.manda_nomes(lista_nomes=lista_nomes, teste=manda3, escolha=1)
 
             for i in range(9):
-                while True:
-                    lista_posicao.clear()
-                    escolha_jogador = jogo_objeto.escolha_jogador
+                lista_posicao.clear()
+                escolha_jogador = jogo_objeto.escolha_jogador
 
-                    if escolha_jogador % 2 == 0:
-                        print("Vez do jogador {}".format(lista_nomes[escolha_jogador % 2]))
-                        recebe4 = mqtt_objeto.connect_mqtt()
-                        mqtt_objeto.recebe_posicao(teste=recebe4, lista_posicao=lista_posicao)
+                if escolha_jogador % 2 == 0:
+                    print("Vez do jogador {}".format(lista_nomes[escolha_jogador % 2]))
+                    recebe4 = mqtt_objeto.connect_mqtt()
+                    mqtt_objeto.recebe_posicao(teste=recebe4, lista_posicao=lista_posicao)
 
-                    elif escolha_jogador % 2 == 1:
-                        print("Vez do jogador {}".format(lista_nomes[escolha_jogador % 2]))
-                        manda4 = mqtt_objeto.connect_mqtt()
-                        mqtt_objeto.manda_posicao(lista_posicao=lista_posicao, teste=manda4, escolha=2)
+                elif escolha_jogador % 2 == 1:
+                    print("Vez do jogador {}".format(lista_nomes[escolha_jogador % 2]))
+                    posicao = jogo_objeto.teste_posicao
+                    manda4 = mqtt_objeto.connect_mqtt()
+                    mqtt_objeto.manda_posicao(lista_posicao=lista_posicao, teste=manda4, escolha=2, msg=posicao)
 
-                    posicao = lista_posicao[0]
-                    jogo_objeto.jogar_mqtt(posicao=posicao)
-                    jogo_objeto.verificar_jogo()
-                    jogo_objeto.decidindo_print()
+                posicao = lista_posicao[0]
+                jogo_objeto.jogar_mqtt(posicao=posicao)
+                jogo_objeto.verificar_jogo()
+                jogo_objeto.decidindo_print(jogador1=lista_nomes[1], jogador2=lista_nomes[0])
 
+            jogo_objeto.print_jogo()  
+            time.sleep(2)
             os.system('cls')
 
         elif choose == 4:
